cf2db5c11aaab5cd44e8c67568389a727726a597
Claudiu Campeanu <claudiu.campeanu@circular.engineering>
2025-12-10 09:30:31 +0100

    fix: Add ".docusaurus" to default excluded directories

--- novaeco-cli/src/novaeco_cli/commands/export.py
+++ novaeco-cli/src/novaeco_cli/commands/export.py
@@ -6,7 +6,7 @@ import sys
 
 DEFAULT_EXCLUDE_DIRS = {
     ".git", ".pytest_cache", "node_modules", "dist", "build", 
-    "__pycache__", ".idea", ".vscode", ".venv", "venv", "bin", "obj"
+    "__pycache__", ".idea", ".vscode", ".venv", "venv", "bin", "obj", ".docusaurus"
 }
 
 DEFAULT_EXCLUDE_EXTS = {
